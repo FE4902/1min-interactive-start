c1329c772266b9b323d907031a4ed8d3dd5c690e
FE4902 <isdn000123@gmail.com>
2024-10-22 23:26:02 +0900

    Nuxt Routing

---
+++ pages/detail/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>Detail 페이지 입니다.</div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>Main 페이지 입니다.</div>
+</template>
+
+<script setup lang="ts"></script>
